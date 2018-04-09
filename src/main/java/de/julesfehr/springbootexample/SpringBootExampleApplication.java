29d55314aaa833c7649f38250d80347caf5d9386
Jules Fehr <fehr.jules@gmail.com>
2018-04-09 19:31:36 +0200

    Initial commit

---
+++ src/main/java/de/julesfehr/springbootexample/SpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package de.julesfehr.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+}
