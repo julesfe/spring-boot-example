1caa15783d4d95b09245ca09fc55a5b09e4fba2f
Jules Fehr <fehr.jules@gmail.com>
2018-04-09 19:53:07 +0200

    Added REST controller

--- src/main/java/de/julesfehr/springbootexample/web/HelloWorldController.java
+++ src/main/java/de/julesfehr/springbootexample/web/HelloWorldController.java
@@ -1,4 +1,18 @@
 package de.julesfehr.springbootexample.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// Define as RestController with Annotation
+@RestController
 public class HelloWorldController {
+
+    // @RequestMapping maps the method to a specific API path
+    // @RequestParam takes parameter from URL: ‘http://localhost:8080/hello?name=World’ will show hello world message
+    @RequestMapping("/hello")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+
 }
